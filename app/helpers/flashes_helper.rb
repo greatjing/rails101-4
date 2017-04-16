26c431344ed3d7ff6918b1fff3fd51ce190bebe0
greatjing <greatjing@sina.cn>
2017-04-16 14:58:36 +0800

    update warining to warning

--- app/helpers/flashes_helper.rb
+++ app/helpers/flashes_helper.rb
@@ -1,5 +1,5 @@
-module FlashesHelpser
-  FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warining"}.freeze
+module FlashesHelper
+  FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
 
   def flash_class(key)
     FLASH_CLASSES.fetch key.to_sym, key
